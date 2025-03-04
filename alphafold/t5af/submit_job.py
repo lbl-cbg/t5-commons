66f1b201ea61c71b132ef199d7de712e82fa199a
ajtritt <ajtritt@lbl.gov>
2025-03-04 12:44:30 -0800

    A couple things
    - Use environment variables to set MSA DB directory and AF weights
    - Submit jobs and update issue by default. Add flags for stopping this

--- alphafold/t5af/submit_job.py
+++ alphafold/t5af/submit_job.py
@@ -20,9 +20,17 @@ def write_fasta(sequence, description, filename):
             fasta_file.write(sequence[i:i+60] + '\n')
 
 def main():
-    parser = argparse.ArgumentParser(description="Poll Jira projects and run a script for each issue.")
+    epi = """The following environment variables must be set:
+
+    MSA_DB_DIR  - The path to the MSA database
+    AF2_WEIGHTS - The path to the AlphaFold2 weights, as downloaded by ColabFold
+    """
+    parser = argparse.ArgumentParser(description="Run AlphaFold using ColabFold for a protein indicated in a Jira issue",
+                                     epilog=epi,
+                                     formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('issue', type=str, help='The issue to process')
-    parser.add_argument('-s', '--submit', action='store_true', help='Submit jobs to Slurm', default=False)
+    parser.add_argument('--no-submit', action='store_true', help='Do not submit jobs to Slurm', default=False)
+    parser.add_argument('--no-jira', action='store_true', help='Do not update Jira', default=False)
     args = parser.parse_args()
 
     jc = JiraConnector()
@@ -40,7 +48,6 @@ def main():
     # Set up job for MSA
     msa_job = SlurmJob(project='m4521', jobname=f"t5af_msa__{key}", output="msa.%J.log", error="msa.%J.log", time="02:00:00")
     msa_job.set_env_var('MMSEQS_PATH', "mmseqs")
-    msa_job.set_env_var('MSA_DB_DIR', "$CFS/m4521/resources/colabfold/msa_db")
     msa_job.set_env_var('MSA_OUTPUT_DIR', "msa")
     msa_job.set_env_var('NCORES', "256")
     msa_job.set_env_var('INPUT', "input.fasta")
@@ -49,9 +56,10 @@ def main():
     msa_sh = "msa.sh"
     with open(msa_sh, 'w') as f:
         msa_job.write(f)
-    msa_job_id = msa_job.submit_job(msa_sh) if args.submit else '0000000'
+    msa_job_id = '0000000' if args.no_submit else msa_job.submit_job(msa_sh)
 
-    # jc.add_comment(args.issue, f"ColabFold MSA job submitted to Perlmutter. Job ID {msa_job_id}")
+    if not args.no_jira:
+        jc.add_comment(args.issue, f"ColabFold MSA job submitted to Perlmutter. Job ID {msa_job_id}")
 
     # Set up job for AlphaFold
     fold_job = SlurmJob(project='m4521', jobname=f"t5af_fold__{key}", output="fold.%J.log", error="fold.%J.log", gpus=1, queue='shared', time="02:00:00")
@@ -60,18 +68,17 @@ def main():
     fold_job.set_env_var('MSA_FILE', os.path.join("msa", f"{name}.a3m"))
     fold_job.set_env_var('PREDICTION_DIR', "prediction")
 
-    fold_job.set_env_var('CUDA_VISIBLE_DEVICES', '$(($JOB_NUMBER % 4))')
-    fold_job.add_command('colabfold_batch --save-all --save-recycles --num-recycle=5 ${MSA_FILE} ${PREDICTION_DIR}')
+    fold_job.add_command('colabfold_batch --data ${AF2_WEIGHTS} --save-all --save-recycles --num-recycle=5 ${MSA_FILE} ${PREDICTION_DIR}')
     fold_job.add_command('mark-job finished')
 
     fold_sh = "fold.sh"
     with open(fold_sh, 'w') as f:
         fold_job.write(f)
-    fold_job_id = fold_job.submit_job(fold_sh) if args.submit else '1111111'
+    fold_job_id = '1111111' if args.no_submit else fold_job.submit_job(fold_sh)
 
-    # jc.add_comment(args.issue, f"ColabFold prediction job submitted to Perlmutter. Job ID {fold_job_id}")
-
-    # jc.transition_issue(args.issue, "10054")   # "In Progress" status id is 10054
+    if not args.no_jira:
+        jc.add_comment(args.issue, f"ColabFold prediction job submitted to Perlmutter. Job ID {fold_job_id}")
+        jc.transition_issue(args.issue, 181)   # "In Progress" status id is 181
 
 
 if __name__ == '__main__':
