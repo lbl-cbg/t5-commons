4630e16760c3ae4929f039ac647c17a54f52adfb
ajtritt <ajtritt@lbl.gov>
2025-06-13 11:23:04 -0700

    Move SLURM env var check

--- jira_automation/scripts/scrontab.perlmutter.t5_jwf_publish.sh
+++ jira_automation/scripts/scrontab.perlmutter.t5_jwf_publish.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-# t5 jwf
+# t5 jwf publish
 #SCRON -q cron
 #SCRON -C cron
 #SCRON -A m4521
@@ -11,8 +10,4 @@
 #SCRON --output=/global/cfs/cdirs/m4521/jira_automation/t5_jwf.perlmutter.%j.log
 #SCRON --error=/global/cfs/cdirs/m4521/jira_automation/t5_jwf.perlmutter.%j.log
 #SCRON --open-mode=append
-if [[ ! -z "${SLURM_MEM_PER_CPU}" ]]; then
-    unset SLURM_MEM_PER_CPU
-    unset SLURM_OPEN_MODE
-fi
 */5 * * * * bash /global/common/software/m4521/jira_automation/scripts/t5_jwf.sh publish /global/common/software/m4521/t5wf
--- jira_automation/scripts/t5_jwf.sh
+++ jira_automation/scripts/t5_jwf.sh
@@ -4,6 +4,11 @@ CMD=${1?"Please specify the t5 jwf command to run"}
 ENV=${2?"Please provide the environment to load before running"}
 CONFIG=${3-"config.yaml"}
 
+if [[ ! -z "${SLURM_MEM_PER_CPU}" ]]; then
+    unset SLURM_MEM_PER_CPU
+    unset SLURM_OPEN_MODE
+fi
+
 module load python
 conda activate $ENV
 
